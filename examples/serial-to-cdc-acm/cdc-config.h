d306ce0a2afb1fd32c8fb6ea37b7dad7c30a2f17
Tim 'mithro' Ansell <mithro@mithis.com>
2015-07-17 22:01:27 +1000

    Refactoring the code.
     * Splitting CDC functionality out into own files.
     * Use proper structures and better defines.
     * Removing the test.cpp file.
     * Change output name.

---
+++ examples/serial-to-cdc-acm/cdc-config.h
@@ -0,0 +1,12 @@
+// The endpoint which data is sent **from** the host down to the CDC serial
+// port.
+#define CDC_IN_ENDPOINT \
+  EP2
+// The endpoint which data is sent **to** the host from the CDC serial port.
+#define CDC_OUT_ENDPOINT \
+  EP6
+
+// The endpoint which is used to notify the host about the CDC state changes.
+#define CDC_INT_ENDPOINT \
+  EP1OUT
+
