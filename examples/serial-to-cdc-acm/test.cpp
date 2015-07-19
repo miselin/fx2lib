bdf6189ac20dae84f93605a562ce6b9bb7c2ca25
Tim 'mithro' Ansell <mithro@mithis.com>
2015-07-19 15:27:51 +1000

    Get the test.cpp program to send/receive data via the UART.

--- examples/serial-to-cdc-acm/test.cpp
+++ examples/serial-to-cdc-acm/test.cpp
@@ -15,9 +15,88 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  **/
+
+#include <stdlib.h>
 #include <cstdio>
 #include <cassert>
 #include <libusb-1.0/libusb.h>
+
+#include <termios.h>
+#include <unistd.h>
+
+
+int kbhit()
+{
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+#define NB_ENABLE 1
+#define NB_DISABLE 2
+
+void nonblock(int state)
+{
+    struct termios ttystate;
+ 
+    //get the terminal state
+    tcgetattr(STDIN_FILENO, &ttystate);
+ 
+    if (state==NB_ENABLE)
+    {
+        //turn off canonical mode
+        ttystate.c_lflag &= ~ICANON;
+        //minimum of number input read.
+        ttystate.c_cc[VMIN] = 1;
+    }
+    else if (state==NB_DISABLE)
+    {
+        //turn on canonical mode
+        ttystate.c_lflag |= ICANON;
+    }
+    //set the terminal attributes.
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+ 
+}
+
+/* Use this variable to remember original terminal attributes. */
+struct termios saved_attributes;
+
+void
+reset_input_mode (void)
+{
+  tcsetattr (STDIN_FILENO, TCSANOW, &saved_attributes);
+}
+
+void
+set_input_mode (void)
+{
+  struct termios tattr;
+  char *name;
+
+/* Make sure stdin is a terminal. */
+  if (!isatty (STDIN_FILENO))
+    {
+      fprintf (stderr, "Not a terminal.\n");
+      exit (1);
+    }
+
+/* Save the terminal attributes so we can restore them later. */
+  tcgetattr (STDIN_FILENO, &saved_attributes);
+  atexit (reset_input_mode);
+
+/* Set the funny terminal modes. */
+  tcgetattr (STDIN_FILENO, &tattr);
+  tattr.c_lflag &= ~(ICANON | ECHO);	/* Clear ICANON and ECHO. */
+  tattr.c_cc[VMIN] = 1;
+  tattr.c_cc[VTIME] = 0;
+  tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
+}
 
 int main(int argc, char* argv[]) {
 
@@ -28,27 +107,31 @@ int main(int argc, char* argv[]) {
  libusb_claim_interface(hndl,0);
  libusb_set_interface_alt_setting(hndl,0,0);
  
- unsigned short buf[100];
- for (int i=0;i<100;++i) {
-  buf[i]=i;
- }
- int transferred;
- int rv=libusb_bulk_transfer(hndl,0x02,(unsigned char*)buf,sizeof(buf),&transferred,100);
- if(rv) {
-  printf ( "OUT Transfer failed: %d\n", rv );
-  return rv;
- }
+ nonblock(NB_ENABLE); set_input_mode();
 
- unsigned short buf2[100];
- rv=libusb_bulk_transfer(hndl,0x86,(unsigned char*)buf2,sizeof(buf2),&transferred,100); 
- if(rv) {
-  printf ( "IN Transfer failed: %d\n", rv );
-  return rv;
+ int rv, transferred;
+
+ setbuf(stdout, NULL);
+
+ while (true) {
+  if (kbhit()) {
+   unsigned char outbuf = fgetc(stdin);
+   rv=libusb_bulk_transfer(hndl,0x02,&outbuf,1,&transferred,100);
+   if(rv) {
+    printf ( "OUT Transfer failed: %d\n", rv );
+    return rv;
+   }
+  } 
+
+  unsigned char inbuf = '\0';
+  rv=libusb_bulk_transfer(hndl,0x86,&inbuf,1,&transferred,100);
+  if(rv) {
+   continue;
+   printf ( "IN Transfer failed: %d\n", rv );
+   return rv;
+  }
+  printf ( "%c", inbuf );
  }
- for (int i=0;i<100;++i) {
-  printf ( "%d ", buf2[i] );
- }
- printf("\n");
 
  return 0;
 }
