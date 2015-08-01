8a0fff4a189d6c32af75b2a17f4f1ae8f3b7ce02
Tim 'mithro' Ansell <mithro@mithis.com>
2015-08-01 12:32:30 +1000

    Adding CDC-ACM loopback firmware and refactoring.

---
+++ examples/cdc/loopback/loopback.c
@@ -0,0 +1,13 @@
+#include "cdc.h"
+
+BOOL cdcuser_set_line_rate(DWORD baud_rate) {
+	return TRUE;
+}
+
+void cdcuser_receive_data(BYTE* data, WORD length) {
+        WORD i;
+        for (i=0; i < length ; ++i) {
+		cdc_queue_data(data[i]);
+	}
+	cdc_send_queued_data();
+}
